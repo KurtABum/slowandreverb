442d9cc7796b212718e4ebe7eb0bbd34c69115ef
KurtABum <kossie518@gmail.com>
2025-10-22 16:11:11 +1000

    Initial Commit

---
+++ slowedandreverb/slowedandreverbApp.swift
@@ -0,0 +1,17 @@
+//
+//  slowedandreverbApp.swift
+//  slowedandreverb
+//
+//  Created by Kurt McCullough on 22/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct slowedandreverbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
